cd10e407123722fba0acc6395afc055b4c054b23
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com>
2025-02-01 23:58:18 +0500

    JS

---
+++ 002_Var, let and const.js
@@ -0,0 +1,13 @@
+var a = 5;
+var b = 5;
+var c = a+b;
+console.log(c);
+// Var is a global variable and should be avoid since it can introduce bugs in the Code.
+
+let d = `Mani`;
+console.log(d);
+// let is a preferable variable and can be used in place of var 
+
+const developer = `CodeBender`;
+console.log(developer);
+// const is used for the identifiers that can't be changed throughout the code
