713a3b1c11572010d86d5b104ce6daaa1ae6c7d5
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com>
2025-02-05 23:23:34 +0500

    JS

---
+++ 009_While Loops.js
@@ -0,0 +1,34 @@
+// There are 2 types of While Loops
+// 1.While Loop
+// 2.Do-While Loop
+
+// --> While Loop
+
+// --> A Sample Program To Print Numbers through While Loop
+
+let f = prompt("Enter Your Number");
+let i = 0;
+while (i<=f){
+    console.log(alert(i));
+    i++;
+}
+
+// --> A program to find even numbers through While Loop
+
+let n = 0;
+while (n<=100){
+    if(n%2==0)
+    console.log(n)
+n++;
+}
+
+// --> Do While Loop
+
+// --> A program to find odd numbers through While Loop
+
+let num = 0;
+do{
+    if(num%2!=0)
+    console.log(num);
+num++;
+}while(num<=100);
