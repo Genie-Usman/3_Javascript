ec195ebf7a0ca3c6dcaa14de81614a91e13e1256
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com>
2025-02-25 23:21:34 +0500

    JS

---
+++ 012_Strings.js
@@ -0,0 +1,21 @@
+// --> Double Quote
+
+let a = "Mani";
+console.log(a);
+
+// --> Single Quote
+
+let b = 'Usman';
+console.log(b);
+
+// --> Template Literal (Backtick)
+
+let c = `Chand`;
+console.log(c);
+
+// --> Escape Sequences
+
+// --> \n
+// --> \r
+// --> \t
+// and many more......
