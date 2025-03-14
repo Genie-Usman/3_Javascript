dbd78b64d22e93ffbfea23019b95ba825957806e
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com>
2025-03-14 21:07:44 +0500

    JS

---
+++ 022_Console Object Methods.js
@@ -0,0 +1,29 @@
+// Some Console Object Methods are:
+
+// 1. Console.Log
+Syntax : console.log("Hello World");
+
+// 2. Console.Error
+Syntax : console.error("Error");
+
+// 3. Console.Warning
+Syntax : console.warn("Warning");
+
+// 4. Console.Table
+object = {a : 2 , b : 3, c : 3};
+Syntax : console.table(object);
+
+// 5. Console.Info
+Syntax : console.info("Info");
+
+// 6. Console.Clear
+Syntax : console.clear("Clear");
+
+// 7. Console.Assert
+Syntax : console.assert("Assert");
+
+// 8. Console.Time
+Syntax : console.time("Time");
+
+// 9. Console.TimeEnd
+Syntax : console.timeEnd("TimeEnd");
